f1ed8a16dab7adce34add495f58d2d1f29eeeab8
Victor Ramirez <vicdeveloper83@gmail.com>
2022-04-25 22:50:02 +0100

    react

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 export default function Card(props) {
+  // {max: 300.15, min: 300.15, name: 'Cairns', img: '03n'}
   // acá va tu código
-  return <div>Card Component</div>
+  return(
+  <div>
+    <input type="button" value="x" onClick={props.onClose}/>
+    <h2>{props.name}</h2>{/* //?city name*/}
+    <div>
+      <div className='max'>
+        <h2>Max</h2>
+        <h3>{props.max}</h3>
+      </div>
+      <div className='min'>
+        <h2>Min</h2>
+        <h3>{props.min}</h3>
+      </div>
+      <div className='icon'>
+        <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="" srcset="" />
+      </div>
+    </div>
+  </div>
+  )
 };
